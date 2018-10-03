56f8bbd60809e26e4359220f63914da8cbc2bfa4
Viktor <malesevicviktor@gmail.com>
2018-10-03 17:52:59 +0200

    Added setup.py and modified .gitignore

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="harry_potter_logistic_regression",
+    version="0.0.1",
+    author="Darren, Viktor",
+    author_email="viktor.malesevic@hec.edu",
+    description="harry potter logistic regression",
+    url="https://github.com/ViktorMalesevic/Ecole42_logistic_regression",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
