d0ef62a57083ef40f61843ce79c904fc6e6a6ec6
Viktor <malesevicviktor@gmail.com>
2018-10-04 14:31:09 +0200

    issues when running in shell

--- harry_potter_logistic_regression/describe.py
+++ harry_potter_logistic_regression/describe.py
@@ -4,12 +4,14 @@ import os
 import sys
 import numpy as np
 import matplotlib as mpl
-from . import utils
-from .utils import fmtp
+#from . import utils
+import utils
+#import fmtp
 from tabulate import tabulate
 
 
-def describe(filepath):
+def describe():
+    filepath = get_filepath()
     data = np.genfromtxt(filepath, delimiter=',', skip_header=1)
     count = np.count_nonzero(data, axis=1)
     mean = np.mean(data, axis=1)
@@ -21,14 +23,24 @@ def describe(filepath):
     max = np.max(data, axis=1)
 
     print('Summary:')
-    display_data = [[fmtp(count), fmtp(mean), fmtp(std),
-                     fmtp(min), fmtp(perc_25),
-                     fmtp(perc_50), fmtp(perc_75),
-                     fmtp(max)]]
+    display_data = [[utils.fmtp(count), utils.fmtp(mean), utils.fmtp(std),
+                     utils.fmtp(min), utils.fmtp(perc_25),
+                     utils.fmtp(perc_50), utils.fmtp(perc_75),
+                     utils.fmtp(max)]]
     print(tabulate(display_data, headers=['Count', 'Mean', 'Standard Dev.',
                                           'Min', '25% Perc.',
                                           '50% Perc.', '75% Perc.',
                                           'Max']))
 
 
+def get_filepath():
+    filepath = sys.argv[1]
+    dirname = os.path.dirname('/Users/vmalesev/Desktop/Ecole42_logistic_regression/data')
+    filepath = os.path.join(dirname, filepath)
+
+    return filepath
+
+
+
+#if __name__ == "__main__":
 describe()
