edfea80757676746542d0fef939e4686d224dd83
Viktor <malesevicviktor@gmail.com>
2018-10-04 11:16:09 +0200

    _read_data and _calculate

--- harry_potter_logistic_regression/describe.py
+++ harry_potter_logistic_regression/describe.py
@@ -6,33 +6,49 @@ import numpy as np
 import matplotlib as mpl
 
 
-data = np.genfromtxt(input('Please provide the file path'), delimiter=',', skip_header=1)
-
-# Script option
-
-count = np.count_nonzero(data, axis=1)
-mean = np.mean(data, axis=1)
-std = np.std(data, axis=1)
-min = np.min(data, axis=1)
-perc_25 = np.percentile(data, 0.25, axis=1)
-perc_50 = np.percentile(data, 0.5, axis=1)
-perc_75 = np.percentile(data, 0.75, axis=1)
-max = np.max(data, axis=1)
-
-
 # Class option
 class Describe(object):
 
-    def __init__(self, data):
+    def __init__(self, filepath):
         super(Describe, self).__init__()
-        self.data = data
+        self.filepath = filepath
+        self._read_data(self.filepath)
+        self._calculate(self.data)
 
-    def calculate
+    def _read_data(self, filepath):
+        self.data = np.genfromtxt(filepath, delimiter=',', skip_header=1)
 
+    def _calculate(self, data):
+        # default values
+        self.count = np.nan
+        self.mean = np.nan
+        self.std = np.nan
+        self.min = np.nan
+        self.perc_25 = np.nan
+        self.perc_50 = np.nan
+        self.perc_75 = np.nan
+        self.max = np.nan
 
+        count = np.count_nonzero(data, axis=1)
+        mean = np.mean(data, axis=1)
+        std = np.std(data, axis=1)
+        min = np.min(data, axis=1)
+        perc_25 = np.percentile(data, 0.25, axis=1)
+        perc_50 = np.percentile(data, 0.5, axis=1)
+        perc_75 = np.percentile(data, 0.75, axis=1)
+        max = np.max(data, axis=1)
 
-
+        self.count = count
+        self.mean = mean
+        self.std = std
+        self.min = min
+        self.perc_25 = perc_25
+        self.perc_50 = perc_50
+        self.perc_75 = perc_75
+        self.max = max
 
 
 if __name__=="__main__":
+    filepath = sys.argv[1]
     main()
+    filepath.Describe()
